553c720bdb3b7325c004f78ca61a231a5a1bb20c
TDreimen <s1290097@u-aizu.ac.jp>
2022-07-08 12:00:07 +0900

    HoT: initial revision

---
+++ heads_or_tails.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+class heads_or_tails{
+    public static void main(String args[]){
+
+	int c1 = 0, c2 = 0;
+
+	for(int i = 0; i < 3; i++){
+	    Random rand = new Random();
+	    int n = rand.nextInt(2);
+	    if(n == 0){
+		System.out.println("Round " + i + ": Heads");
+		c1++;
+	    }else{
+		System.out.println("Round " + i + ": Tails");
+		c2++;
+	    }
+	}
+
+	System.out.println("Heads: " +c1+ ", Tails: " +c2);
+	
+    }
+}
